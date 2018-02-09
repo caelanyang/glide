6ffbf61f3a02bd5958904999376768acd6bcf805
caelanyang <caelan.yang@gmail.com>
2018-02-09 20:23:36 +0800

    一些格式变化

--- library/src/main/java/com/bumptech/glide/request/target/ImageViewTargetFactory.java
+++ library/src/main/java/com/bumptech/glide/request/target/ImageViewTargetFactory.java
@@ -10,17 +10,17 @@ import android.widget.ImageView;
  * {@link com.bumptech.glide.request.target.Target} for a given {@link android.view.View} subclass.
  */
 public class ImageViewTargetFactory {
-  @NonNull
-  @SuppressWarnings("unchecked")
-  public <Z> ViewTarget<ImageView, Z> buildTarget(@NonNull ImageView view,
-      @NonNull Class<Z> clazz) {
-    if (Bitmap.class.equals(clazz)) {
-      return (ViewTarget<ImageView, Z>) new BitmapImageViewTarget(view);
-    } else if (Drawable.class.isAssignableFrom(clazz)) {
-      return (ViewTarget<ImageView, Z>) new DrawableImageViewTarget(view);
-    } else {
-      throw new IllegalArgumentException(
-          "Unhandled class: " + clazz + ", try .as*(Class).transcode(ResourceTranscoder)");
+    @NonNull
+    @SuppressWarnings("unchecked")
+    public <Z> ViewTarget<ImageView, Z> buildTarget(@NonNull ImageView view,
+                                                    @NonNull Class<Z> clazz) {
+        if (Bitmap.class.equals(clazz)) {
+            return (ViewTarget<ImageView, Z>) new BitmapImageViewTarget(view);
+        } else if (Drawable.class.isAssignableFrom(clazz)) {
+            return (ViewTarget<ImageView, Z>) new DrawableImageViewTarget(view);
+        } else {
+            throw new IllegalArgumentException(
+                    "Unhandled class: " + clazz + ", try .as*(Class).transcode(ResourceTranscoder)");
+        }
     }
-  }
 }
